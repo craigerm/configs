9046101bd8f1029cde82ccfe4be6ada0d31b09b9
craigerm <craigerm@gmail.com>
2013-07-17 00:49:48 -0400

    Install will include some other special files

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir ~/configs -p
 # Sym link all aliases to here
 for file in .*aliases
 do
-   echo "Adding alias file ${file}."
+   echo "Linking alias file ${file}."
    rm "$HOME/configs/${file}" -f
    ln -s "$PWD/${file}" "$HOME/configs/${file}"
 done
@@ -14,7 +14,17 @@ done
 # Sym link all rc files
 for file in .*rc
 do
-  echo "Adding rc file ${file}."
+  echo "Linking rc file ${file}."
+  rm "$HOME/${file}" -f
+  ln "$PWD/${file}" "$HOME/${file}"
+done
+
+SPECIAL_FILES=".gitk .gitconfig .bash_functions"
+
+# Sys link any other files
+for file in $SPECIAL_FILES
+do
+  echo "Linking special file ${file}."
   rm "$HOME/${file}" -f
   ln "$PWD/${file}" "$HOME/${file}"
 done
