a12bdc706e6a450b510047e76e415239097c0d22
Craig MacGregor <craigerm@gmail.com>
2023-04-20 05:47:55 -0400

    Change null-ls to use correct typescript source

--- nvim/pack-config/null-ls.lua
+++ nvim/pack-config/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 local sources = {
   null_ls.builtins.formatting.prettierd,
-  null_ls.builtins.diagnostics.ts,
+  null_ls.builtins.diagnostics.tsc,
 }
 
 null_ls.setup({
