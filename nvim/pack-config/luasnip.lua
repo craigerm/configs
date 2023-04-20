c3aff03322528afee959c7693eb64ba45f2154b5
Craig MacGregor <craigerm@gmail.com>
2023-04-20 05:21:06 -0400

    Minor changes

--- nvim/pack-config/luasnip.lua
+++ nvim/pack-config/luasnip.lua
@@ -1,7 +1,6 @@
 local ls = require('luasnip')
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_snipmate").lazy_load()
 
 -- require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/typescript" } }
+require("luasnip.loaders.from_snipmate").lazy_load()
 
 ls.filetype_extend("all", { "_" })
--- nvim/pack-config/null-ls.lua
+++ nvim/pack-config/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls = require("null-ls")
 
 local sources = {
-  null_ls.builtins.formatting.prettierd,
+  null_ls.builtins.formatting.prettier,
   null_ls.builtins.diagnostics.ts,
 }
 
