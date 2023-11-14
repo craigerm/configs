1b5f69117c0af6daf7ea59dac0aee2256897f592
Craig MacGregor <craigerm@gmail.com>
2023-11-14 01:49:46 -0500

    Hide whitespace on lsp info window but show in zen mode

--- nvim/pack-config/zen-mode.lua
+++ nvim/pack-config/zen-mode.lua
@@ -1,2 +1,13 @@
+local zen = require("zen-mode")
+
 local map = vim.api.nvim_set_keymap
 map('n', '<F9>', ':ZenMode<cr>', {})
+
+zen.setup({
+  window = {
+    options = {
+      -- Enable whitespace characters
+      list = true
+    }
+  }
+})
