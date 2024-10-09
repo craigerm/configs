11635696981021048f7d77706f0f884c3235de11
Craig MacGregor <craigerm@gmail.com>
2024-10-09 19:03:11 -0400

    Fixes for trouble

--- nvim/pack-config/trouble.lua
+++ nvim/pack-config/trouble.lua
@@ -6,9 +6,10 @@ trouble.setup({})
 local opts = { noremap = true, silent = true }
 
 -- Mappings
-map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", opts)
+map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", opts)
+map("n", "<leader>xp", "<cmd>Trouble diagnostics toggle<cr>", opts)
 map("n", "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", opts)
-map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
+map("", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", opts)
 map("n", "<leader>xd", "<cmd>Trouble symbols toggle focus=true<cr>", opts)
 map(
   "n",
