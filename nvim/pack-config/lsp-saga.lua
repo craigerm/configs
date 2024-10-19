475731d2df52611ccd0d8d7a2d268e4cfb334c96
Craig MacGregor <craigerm@gmail.com>
2024-10-18 23:04:40 -0400

    Minor stuff for lspsaga

--- nvim/pack-config/lsp-saga.lua
+++ nvim/pack-config/lsp-saga.lua
@@ -23,23 +23,21 @@ end
 local opts = { silent = true }
 
 -- Finder/Hover info
-map('n', '<leader>h', '<cmd>Lspsaga finder<CR>', opts)
-map('n', 'gd', '<cmd>Lspsaga peek_definition<CR>', opts)
-map('n', '<leader>g', '<cmd>Lspsaga goto_definition<CR>', opts)
-map('n', 'K', '<cmd>Lspsaga hover_doc<cr>', opts)
-map('n', 'gt', '<cmd>Lspsaga goto_type_definition<CR>', opts)
+map("n", "<leader>h", "<cmd>Lspsaga finder<CR>", opts)
+map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+map("n", "<leader><space>", "<cmd>Lspsaga goto_definition<CR>", opts)
+map("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+map("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>", opts)
 
 -- Code actions
-map('', '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
-map('n', '<F2>', '<cmd>Lspsaga rename<CR>', opts)
+map("", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+map("n", "<F2>", "<cmd>Lspsaga rename<CR>", opts)
 
 -- Diagnostics
-map('n', '<leader>cf', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
-map('n', '<leader>cd', '<cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
-map('n', '[e', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
-map('n', ']e', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+map("n", "<leader>cf", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+map("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 
 -- Outline LSoutline is deprecated but new versions are not working correctly
-map('n', '<leader>o', '<cmd>Lspsaga outline<cr>', opts)
-
--- Experiments
+map("n", "<leader>o", "<cmd>Lspsaga outline<cr>", opts)
