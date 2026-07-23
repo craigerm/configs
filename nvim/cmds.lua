05d7a2c4fa0dcfb2d1a48e6968b8621a075af865
Craig MacGregor <craigerm@gmail.com>
2026-07-23 16:57:42 -0400

    Highlight some env files, etc.

--- nvim/cmds.lua
+++ nvim/cmds.lua
@@ -9,3 +9,45 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.filetype.add({
+  filename = {
+    [".dev.vars"] = "sh",
+    [".dev.vars.test"] = "sh",
+    ["dev.vars.example"] = "sh",
+    [".env"] = "sh",
+    [".env.example"] = "sh",
+    ["env-schema"] = "sh",
+  },
+})
+
+vim.api.nvim_set_hl(0, "EnvAnnotation", { fg = "#C586C0", italic = true })
+
+local function add_env_annotation_match()
+  local ft = vim.bo.filetype
+  local name = vim.fn.expand("%:t")
+
+  if ft ~= "sh" and ft ~= "bash" and ft ~= "zsh" and name ~= "env-schema" then
+    return
+  end
+
+  local win_match_id = vim.w.env_annotation_match_id
+  if win_match_id then
+    pcall(vim.fn.matchdelete, win_match_id)
+  end
+
+  vim.w.env_annotation_match_id = vim.fn.matchadd("EnvAnnotation", [[#.*\zs@[A-Za-z0-9_:=-]\+]])
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("env_schema_annotations"),
+  pattern = { "sh", "bash", "zsh" },
+  callback = add_env_annotation_match,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+  group = augroup("env_schema_annotations_windows"),
+  pattern = "*",
+  callback = add_env_annotation_match,
+})
+
