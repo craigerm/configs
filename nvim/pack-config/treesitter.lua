99a7d1ea4922d26a641f399e61107c29e0f8f649
Craig MacGregor <craigerm@gmail.com>
2026-08-12 11:41:07 -0400

    Add back liquid support to ts

--- nvim/pack-config/treesitter.lua
+++ nvim/pack-config/treesitter.lua
@@ -11,13 +11,20 @@ require("nvim-treesitter").install({
   "graphql",
   "markdown",
   "markdown_inline",
+  "liquid",
   "scss",
   "toml",
   "yaml",
   "elixir",
   "heex",
-  "eex"
+  "eex",
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "liquid",
+  callback = function(event)
+    vim.treesitter.start(event.buf)
+  end,
+})
 -- Mappings (this needs migrated to latest treesitter version if possible)
 -- vim.api.nvim_set_keymap("n", "<F10>", ":TSHighlightCapturesUnderCursor<cr>", {})
